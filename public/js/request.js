ebdd3f8f6c568b06dd0da84912bc2eeeeeb939b2
Carlos da Costa <carlosdaviddacosta@gmail.com>
2019-07-18 15:06:57 +0200

    fix

--- public/js/request.js
+++ public/js/request.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', e =>{
 
   const endPoint = `https://youtubevideodl.herokuapp.com/download?url=${url}&format=${format}`
   if(!url.includes('youtube.com/watch?'))
-    window.alert("Please provida a valid video url...")
+    window.alert("Please provid a valid youtube video url...")
   else
     window.location.href = endPoint
 })
